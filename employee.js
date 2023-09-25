b38618e011ca05beeff56f2908e953b8e81a88c9
gsusk <satehortuamercado@gmail.com>
2023-09-25 17:32:00 -0500

    chall

--- employee.js
+++ employee.js
@@ -1,1 +1,18 @@
-// Your code here
+class Employee {
+  constructor(name, ocupation) {
+    this.name = name;
+    this.ocupation = ocupation;
+  }
+
+  sayName() {
+    console.log(`${this.name} says hello`);
+  }
+
+  sayOcupation() {
+    console.log(`${this.name} is a ${this.ocupation}`);
+  }
+}
+
+module.exports = {
+  Employee
+}
--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-// Your code here
+const { Employee } = require('./employee');
+
+const employ = new Employee('John Wick', "Dog Lover")
+const sayName = employ.sayName.bind(employ)
+const sayOcup = employ.sayOcupation.bind(employ)
+setTimeout(sayName, 2000)
+setTimeout(sayOcup, 3000)
